03e8c0189d17953b919c9f1da0b50e4ea4ed3cff
kaklakariada <kaklakariada@users.noreply.github.com>
2024-09-21 09:45:17 +0200

    Fix build

--- src/main/java/org/itsallcode/jlua/Test.java
+++
@@ -1,9 +0,0 @@
-package org.itsallcode.jlua;
-
-public class Test {
-    public static void main(final String[] args) {
-        try (LuaInterpreter lua = LuaInterpreter.create()) {
-            lua.exec("print('hello world')");
-        }
-    }
-}
