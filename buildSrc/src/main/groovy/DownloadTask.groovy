32f8169d2ecb0c0b9d9c31f65cb8186d31cb837a
kaklakariada <kaklakariada@users.noreply.github.com>
2024-09-21 10:00:08 +0200

    Enable caching for download task

--- buildSrc/src/main/groovy/DownloadTask.groovy
+++ buildSrc/src/main/groovy/DownloadTask.groovy
@@ -2,7 +2,9 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.CacheableTask
 
+@CacheableTask
 class DownloadTask extends DefaultTask {
     @Input
     String sourceUrl
